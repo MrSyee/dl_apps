fe7c330bcafc4c0d65abdf259831fa7affd3bba8
Kyunghwan <khsyee@gmail.com>
2023-03-30 22:54:47 +0900

    Test ocr page

--- ocr/frontend/frontend.py
+++ ocr/frontend/frontend.py
@@ -15,69 +15,56 @@ else:
     BACKEND_URL = "http://localhost:8888/"
 PREDICT_URL = urllib.parse.urljoin(BACKEND_URL, "infer")
 
+CANVAS_WIDTH = 600
+CANVAS_HEIGHT = 192
+os.makedirs("inputs", exist_ok=True)
 
+# Set title
 st.title("Handwritten image to Text")
 st.markdown("Try to write some sentences!")
 
-CANVAS_WIDTH = 600
-CANVAS_HEIGHT = 192
+# Set canvas
 CanvasResult = st_canvas(
-    fill_color="#000000",
-    stroke_width=5,
-    stroke_color="#FFFFFF",
-    background_color="#000000",
+    fill_color="#FFFFFF",
+    stroke_width=3,
+    stroke_color="#000000",
+    background_color="#FFFFFF",
     width=CANVAS_WIDTH,
     height=CANVAS_HEIGHT,
     drawing_mode="freedraw",
     key="canvas",
 )
 
+# Get handwritten text image fron canvas
 if CanvasResult.image_data is not None:
     img_array = CanvasResult.image_data.astype("uint8")
-
-
-image_file = st.file_uploader("Upload Images", type=["png", "jpg", "jpeg"])
-if image_file is not None:
-    # To See details
-    file_details = {
-        "filename": image_file.name,
-        "filetype": image_file.type,
-        "filesize": image_file.size,
-    }
-    st.write(file_details)
-
-    # Send image to backend
-    files = {"file": image_file.getvalue()}
 
 text_value = ""
 if st.button("Predict"):
     try:
-        # image_byte = img_array.tobytes()
-        # frame_file = io.BytesIO(image_byte)
-
         image = Image.fromarray(img_array)
+        image.save("inputs/canvas.png", format="PNG")
+
         buffered = io.BytesIO()
         image.save(buffered, format="PNG")
         buffered.seek(0)
         image_bytes = buffered.getvalue()
 
         files = {"file": image_bytes}
-        print("files: ", files)
 
-
-        print("PREDICT_URL: ", PREDICT_URL)
+        # Request to API
         response_predict = requests.post(
             url=PREDICT_URL,
             files=files,
         )
 
-        print("response_predict: ", response_predict)
+        # Get text
         res = response_predict.json()
-        print("res: ", res)
         text_value = res['text']
         st.write(f"Prediction: {res['text']}")
 
     except ConnectionError:
         st.write("Couldn't reach backend")
 
+# Set text area
 st.text_area("Result", text_value)
--- ocr/server/app.py
+++ ocr/server/app.py
@@ -25,7 +25,6 @@ def healthcheck() -> bool:
 @app.post("/infer", response_model=OCRResponse, status_code=200)
 async def infer(file: UploadFile) -> OCRResponse:
     """Inference model."""
-    print(file)
     image = Image.open(file.file).convert("RGB")
     text = inferencer.inference(image)
 

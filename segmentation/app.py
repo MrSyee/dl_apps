8511e674d20380fac027f392442a68f04ffe1661
Kyunghwan Kim <khsyee@gmail.com>
2023-04-20 23:18:23 +0900

    Test Gradio click (#3)
    * Test gradio click
    
    * Add ipynb

---
+++ segmentation/app.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+with gr.Blocks() as demo:
+    coords = gr.Textbox(label="Mouse coords")
+    with gr.Row():
+        input_img = gr.Image(label="Input")
+
+    def get_coords(evt: gr.SelectData):
+        return f"(x, y): ({evt.index[1]}, {evt.index[0]})"
+
+    input_img.select(get_coords, None, coords)
+
+if __name__ == "__main__":
+    demo.launch()

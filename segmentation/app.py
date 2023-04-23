98ab3ab9f4fe93de0ca41277529c88be72925613
Kyunghwan <khsyee@gmail.com>
2023-04-24 02:56:17 +0900

    Add examples

--- segmentation/app.py
+++ segmentation/app.py
@@ -59,9 +59,45 @@ def draw_contour(image: np.ndarray, mask: np.ndarray) -> np.ndarray:
     return contour_image, contours
 
 
+def extract_object(
+    image: np.ndarray, point_h: int, point_w: int, point_label: int
+):
+    point_coords = np.array([[point_h, point_w]])
+    point_label = np.array([point_label])
+    # image_pil = Image.fromarray(image).convert("RGB")
+    # image_pil.save("inputs/origin.png", format="PNG")
+
+    # Get mask
+    mask = inferencer.inference(image, point_coords, point_label)
+    overlay_image, contours = draw_contour(image, mask)
+    # overlay_image_pil = Image.fromarray(overlay_image).convert("RGB")
+    # overlay_image_pil.save("inputs/contour.png", format="PNG")
+
+    # Extract object
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, contours, (255, 255, 255))
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    segmented_image = cv2.bitwise_and(image, image, mask=mask)
+
+    return segmented_image
+
+def extract_object_by_event(image: np.ndarray, evt: gr.SelectData):
+    click_h, click_w = evt.index
+
+    return extract_object(image, click_h, click_w, 1)
+
+
+def get_coords(evt: gr.SelectData):
+    return f"(h, w): ({evt.index[0]}, {evt.index[1]})"
+
+
 with gr.Blocks() as demo:
     gr.Markdown("# Interactive Extracting Object from Image")
     coords = gr.Textbox(label="Mouse coords")
+    with gr.Row():
+        coord_h = gr.Number(label="Mouse coords h")
+        coord_w = gr.Number(label="Mouse coords w")
+        click_label = gr.Number(label="label")
 
     with gr.Row():
         input_img = gr.Image(label="Input image").style(height=1000, width=1000)
@@ -69,34 +105,21 @@ with gr.Blocks() as demo:
 
     extract_btn = gr.Button("Extract")
 
-    def extract_object_by_click(image: np.ndarray, evt: gr.SelectData):
-        print("image: ", image.shape)
-        image_pil = Image.fromarray(image).convert("RGB")
-        image_pil.save("inputs/origin.png", format="PNG")
-        click_h, click_w = evt.index
-
-        point_coords = np.array([[click_h, click_w]])
-        points_labels = np.array([1])
-        mask = inferencer.inference(image, point_coords, points_labels)
-        print("mask: ", mask.shape)
-        output_image, contours = draw_contour(image, mask)
-        print("output_image: ", output_image.shape)
-        output_image_pil = Image.fromarray(output_image).convert("RGB")
-        output_image_pil.save("inputs/contour.png", format="PNG")
-
-        # Extract object
-        mask = np.zeros_like(image)
-        cv2.fillPoly(mask, contours, (255, 255, 255))
-        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-        result = cv2.bitwise_and(image, image, mask=mask)
-
-        return result
-
-    def get_coords(evt: gr.SelectData):
-        return f"(x, y): ({evt.index[1]}, {evt.index[0]})"
-
-    input_img.select(extract_object_by_click, [input_img], output_img)
+    input_img.select(extract_object_by_event, [input_img], output_img)
     input_img.select(get_coords, None, coords)
+
+
+
+    gr.Markdown("## Image Examples")
+    gr.Examples(
+        examples=[
+            [os.path.join(os.path.dirname(__file__), "examples/dog.jpg"), 1013, 786, 1]
+        ],
+        inputs=[input_img, coord_h, coord_w, click_label],
+        outputs=output_img,
+        fn=extract_object,
+        run_on_click=True,
+    )
 
 if __name__ == "__main__":
     demo.launch()

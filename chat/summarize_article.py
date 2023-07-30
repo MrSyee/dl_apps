feba54f49df4d33a6490559526e19ebb7e5bd383
Kyunghwan Kim <khsyee@gmail.com>
2023-07-30 18:50:48 +0900

    Add crawl CNN

--- chat/summarize_article.py
+++ chat/summarize_article.py
@@ -2,17 +2,27 @@ import gradio as gr
 import openai
 from dotenv import load_dotenv
 import os
+import requests
+from bs4 import BeautifulSoup
 
 load_dotenv()
 
-example_text = """
-The Ukrainian military is doubling down on efforts to break through thick Russian defenses in its counteroffensive in the south, which has struggled to gain momentum since being launched at the beginning of June.
-Ukrainian officials have said little about what fresh units are being committed to the offensive, but the military has clearly added recently-minted units equipped with western armor in at least one important segment of the southern front.
-The challenges faced by the Ukrainians are perhaps less to do with numbers and more to do with capabilities, training and coordination, factors that are critical when an attacking force is faced with such an array of defenses.
-Fragments of geolocated video show that western armor such as Bradley fighting vehicles have been part of the renewed assault and that experienced units have been brought into the fray. But tight operational security on the part of the Ukrainians precludes a full assessment of what is being done to reboot the counteroffensive – and where.
-There's still debate about the size of the additional effort.
-George Barros of the Institute for the Study of War – a Washington-based group – told CNN: “We had not seen any evidence of a battalion-level attack and certainly no brigade-level attacks. If the Ukrainians are indeed committing full battalions and brigades now as reported, that would mark a clear new phase of the Ukrainian counteroffensive.”
-"""
+
+# cnn 기사 크롤링 함수
+# max 토큰 지정
+def crawl(url: str):
+    rep = requests.get(url)
+
+    soup = BeautifulSoup(rep.content, 'html.parser')
+
+    # Get title
+    # article = soup.find('h1').get_text()
+    # Get main contents
+    article = ""
+    for paragraph in soup.find_all(['p', 'h2'], {"class":['paragraph', "subheader"]}):
+        article += paragraph.text.strip()
+
+    return article
 
 class GPTInferencer:
     def __init__(self):
@@ -20,6 +30,7 @@ class GPTInferencer:
         self.model = "gpt-3.5-turbo"
 
     def summarize(self, texts: str):
+        print(f"input of chatgpt: {texts}")
         query = f"""
             Summarize the sentences below.
             ---
@@ -32,6 +43,7 @@ class GPTInferencer:
 
         # ChatGPT API 호출하기
         response = openai.ChatCompletion.create(model=self.model, messages=messages)
+        print(response)
         return response["choices"][0]["message"]["content"]
 
     def translate(self, texts: str):
@@ -54,12 +66,17 @@ gpt_inferencer = GPTInferencer()
 with gr.Blocks() as app:
     gr.Markdown("# Summarize article & Translate")
     gr.Markdown("## 요약할 기사의 주소를 입력하세요.")
-    url = gr.Textbox(example_text, label="URL")
-    abstract_btn = gr.Button("Abstract!")
+    url = gr.Textbox(label="URL")
+    crawl_btn = gr.Button("Crawl!")
+
+    gr.Markdown("## 원문 기사")
+    original_box = gr.Textbox(label="Original text", lines=7)
+    crawl_btn.click(crawl, inputs=[url], outputs=[original_box])
+    summarize_btn = gr.Button("Summarize!")
 
     gr.Markdown("## 원문 기사 요약")
-    abstract_box = gr.Textbox(label="Abstract", lines=7)
-    abstract_btn.click(gpt_inferencer.summarize, inputs=[url], outputs=[abstract_box])
+    abstract_box = gr.Textbox(label="Summarize", lines=7)
+    summarize_btn.click(gpt_inferencer.summarize, inputs=[original_box], outputs=[abstract_box])
 
 
     gr.Markdown("## 요약문 한국어 번역")
